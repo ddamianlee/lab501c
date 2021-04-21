5dac4b953574c6f90ce9b67ecac279980bc31963
ddamianlee <ddamianlee@gmail.com>
2021-04-21 03:48:44 -0700

    add zpipe

--- layout.h
+++ layout.h
@@ -1,10 +1,10 @@
-#define MAXLEN 4954309
+//#define MAXLEN 99999999
 POBJ_LAYOUT_BEGIN(rstore);
 POBJ_LAYOUT_ROOT(rstore, struct my_root);
 POBJ_LAYOUT_END(rstore);
 
 struct my_root
 {
-	char r[37116];
+	char r[16384];
 };
 
